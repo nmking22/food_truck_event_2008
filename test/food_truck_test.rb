26e33bdb2037b91d8220d4679020baf4af6f4f25
Nick King <nickmaxking@gmail.com>
2020-09-15 13:12:55 -0600

    Add tests for initialize and readable attributes

---
+++ test/food_truck_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+require './lib/food_truck'
+
+class FoodTruckTest < Minitest::Test
+  def test_it_exists
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    assert_instance_of FoodTruck, food_truck
+  end
+
+  def test_it_has_readable_attributes
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    expected = {}
+    assert_equal "Rocky Mountain Pies", food_truck.name
+    assert_equal expected, food_truck.inventory
+  end
+end
