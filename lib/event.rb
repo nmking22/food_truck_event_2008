4e75b08edad187837e342e7ade04b474cbc03238
Nick King <nickmaxking@gmail.com>
2020-09-15 13:47:49 -0600

    Improve food_trucks_that_sell method to account for fringe cases

--- lib/event.rb
+++ lib/event.rb
@@ -18,7 +18,7 @@ class Event
 
   def food_trucks_that_sell(item)
     @food_trucks.find_all do |food_truck|
-      food_truck.inventory.has_key?(item)
+      food_truck.inventory[item] > 0
     end
   end
 end
