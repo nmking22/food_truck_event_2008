e60790d2d85fa5f73b7dacdbf2a8baa5e99ab906
Nick King <nickmaxking@gmail.com>
2020-09-15 13:25:41 -0600

    Add test for stock changing inventory and check_stock

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -25,4 +25,26 @@ class FoodTruckTest < Minitest::Test
 
     assert_equal 0, food_truck.check_stock(item1)
   end
+
+  def test_stock_modifies_check_stock_and_inventory
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+    food_truck.stock(item1, 30)
+    expected_1 = {item1 => 30}
+
+    assert_equal expected_1, food_truck.inventory
+    assert_equal 30, food_truck.check_stock(item1)
+
+    food_truck.stock(item1, 25)
+
+    assert_equal 55, food_truck.check_stock(item1)
+
+    food_truck.stock(item2, 12)
+    expected_2 = {item1 => 55, item2 => 12}
+
+    assert_equal expected_2, food_truck.inventory
+  end
+
 end
